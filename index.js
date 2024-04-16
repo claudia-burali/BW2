cf637829f0a1c44ebc8983c5f5576a498d86a4d7
claudia-burali <161461840+claudia-burali@users.noreply.github.com>
2024-04-16 19:29:45 +0200

    mod

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const albumIds = [
   388425797, 1434890, 127402, 81797, 6364781, 130380032, 428115167, 74606742, 1318764, 8015598, 125584, 14879699,
   36963671, 1262269, 108444952, 10966644,
 ];
+
+const limit = 5;
+
 const URL_ALBUM_BASE = "https://deezerdevs-deezer.p.rapidapi.com/album/";
 
 const fetchAlbum = (albumId) => {
@@ -46,6 +49,55 @@ const fetchAlbum = (albumId) => {
 const album = () => {
   albumIds.forEach((albumId) => {
     fetchAlbum(albumId);
+  });
+};
+
+let artistContainer = document.getElementById("artist");
+const artistIds = [
+  1182, 214810, 1026, 406, 4138, 5286, 12247, 5608864, 4868678, 176639, 534258, 532, 458, 117, 599, 1197801, 331727,
+  1092125, 647650, 464, 98, 407, 399, 3702, 860, 1994, 652,
+];
+const URL_ARTIST_BASE = "https://deezerdevs-deezer.p.rapidapi.com/artist/";
+
+const fetchArtist = (artistId) => {
+  const URL_ARTIST = URL_ARTIST_BASE + artistId;
+
+  fetch(URL_ARTIST, {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "285e755c43mshee6ccde0f56b047p1fd3b7jsn69d102243084",
+      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Errore nella richiesta HTTP");
+      }
+      return response.json();
+    })
+    .then((artist) => {
+      console.log("Artista aggiunto con successo:", artist);
+      let artistCard = document.createElement("div");
+      artistCard.classList.add("d-flex");
+      artistCard.classList.add("gap-5");
+      artistCard.classList.add("p-3");
+      artistCard.classList.add("m-5");
+      artistCard.classList.add("text-light");
+      artistCard.innerHTML = ` 
+  <img src=${artist.picture} alt="${artist.name}">
+  <div> 
+  <h3>${artist.name}</h3> 
+  </div>`;
+      artistContainer.appendChild(artistCard);
+    })
+    .catch((error) => {
+      console.error("Errore:", error);
+    });
+};
+
+const artist = () => {
+  artistIds.forEach((artistId) => {
+    fetchArtist(artistId);
   });
 };
 
@@ -94,4 +146,5 @@ const playlist = () => {
 window.onload = () => {
   album();
   playlist();
+  artist();
 };
